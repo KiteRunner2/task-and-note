eca770f89b9b7ab8e2ca213086e5fbc329d3c18b
Przemek Rudzki <przemek@kritik.io>
2022-02-08 21:23:25 -0500

    Add scheleton for Register component

---
+++ src/components/Register/index.tsx
@@ -0,0 +1,18 @@
+import './Register.css'
+import Button from '../Button'
+function Register() {
+  function handleClick() {
+    console.log('clicked')
+  }
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <input type="text" />
+        <input type="password" />
+        <Button onClick={handleClick}>Register</Button>
+      </div>
+    </div>
+  )
+}
+
+export default Register
