4fe8fe9094ed3f030bf4034a542dce8ed40d3a88
Przemek Rudzki <przemek@rudzki.ca>
2022-02-18 08:59:24 -0500

    add Link to Login in Register component

--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Register.css'
 import Button from '../Button'
+import { Link } from 'react-router-dom'
 function Register() {
   function handleClick() {
     console.log('clicked')
@@ -11,6 +12,7 @@ function Register() {
         <input type="text" />
         <input type="password" />
         <Button onClick={handleClick}>Register</Button>
+        <Link to="/login">Login</Link>
       </div>
     </div>
   )
