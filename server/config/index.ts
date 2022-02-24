19839505500c987993dd86b984d3bcb1d844deb5
Przemek Rudzki <przemek@rudzki.ca>
2022-02-24 09:23:43 -0500

    add additional app configuration vars

--- server/config/index.ts
+++ server/config/index.ts
@@ -2,3 +2,10 @@ require('dotenv').config()
 
 export const { PORT } = process.env
 export const { BUILD_PATH } = process.env
+export const { DB_NAME } = process.env
+export const { DB_PASSWORD } = process.env
+export const { DB_URI_PROD } = process.env
+export const { DB_URI_DEV } = process.env
+export const { ENV } = process.env
+
+export const DB_URI = ENV === 'development' ? DB_URI_DEV : DB_URI_PROD
