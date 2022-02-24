9447b3f7c1eceb928db464a6c24694e29469a0de
Przemek Rudzki <przemek@rudzki.ca>
2022-02-24 09:24:15 -0500

    add db connection when server is starting

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { PORT, BUILD_PATH } from './config/index'
 import testRoute from './routes'
+import { connectDb } from './db'
 
 const app = express()
 
@@ -11,6 +12,8 @@ app.use('/api/v1', testRoute)
 app.get('*', (req, res) => {
   return res.sendFile(`${BUILD_PATH}/index.html`)
 })
+
+connectDb()
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
