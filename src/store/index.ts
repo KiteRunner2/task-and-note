aac57477e0dc134a25e5431b095ea4795288c079
Przemek Rudzki <przemek@rudzki.ca>
2022-02-24 21:06:04 -0500

    add register state to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import create from 'zustand'
 import { devtools } from 'zustand/middleware'
 import authState from './authState'
+import registerState from './registerState'
 
 const store = (set: any, get: any) => ({
   ...authState(set, get),
+  ...registerState(set, get),
 })
 
 const useStore = create(devtools(store))
