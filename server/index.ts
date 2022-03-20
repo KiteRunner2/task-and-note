955ae77f0aa19cd75ebaaebef5faab49bfba33d1
Przemek Rudzki <przemek@rudzki.ca>
2022-03-19 20:11:11 -0400

    initi publick routes

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { PORT, BUILD_PATH, ENV } from './config/index'
-import testRoute from './routes'
+import routes from './routes'
 import { connectDb } from './db'
 
 const app = express()
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.static(BUILD_PATH!))
 app.use(express.json())
 
-app.use('/api/v1', testRoute)
+app.use('/api/v1', routes.initPublicRoutes())
 
 app.get('*', (req, res) => {
   return res.sendFile(`${BUILD_PATH}/index.html`)
